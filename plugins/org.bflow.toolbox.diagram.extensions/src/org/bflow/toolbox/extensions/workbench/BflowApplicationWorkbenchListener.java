3b89b598f35dceceb4d7c1d39356adba763be346
astorch <astorch@ee779243-7f51-0410-ad50-ee0809328dc4>
2015-02-27 17:02:45 +0000

    Refresh the whole workspace at start up
    git-svn-id: svn+ssh://svn.code.sf.net/p/bflowtoolbox/code/trunk@1044 ee779243-7f51-0410-ad50-ee0809328dc4

--- plugins/org.bflow.toolbox.diagram.extensions/src/org/bflow/toolbox/extensions/workbench/BflowApplicationWorkbenchListener.java
+++ plugins/org.bflow.toolbox.diagram.extensions/src/org/bflow/toolbox/extensions/workbench/BflowApplicationWorkbenchListener.java
@@ -1,16 +1,35 @@
 package org.bflow.toolbox.extensions.workbench;
 
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchListener;
 
 /**
- * Implements {@link IWorkbenchListener} to close all open diagrams when 
- * the workbench is closing. 
+ * Implements {@link IWorkbenchListener} to close all open diagrams when the
+ * workbench is closing.
  * 
  * @author Arian Storch<arian.storch@bflow.org>
- * @since 14/12/13
+ * @since 14.12.13
+ * @version 27.02.15 Add feature: refresh workspace at start up
  */
 public class BflowApplicationWorkbenchListener implements IWorkbenchListener {
+	
+	/**
+	 * Creates a new instance. Each instance refreshes the whole workspace on
+	 * initialization.
+	 */
+	public BflowApplicationWorkbenchListener() {
+		// Refresh the whole workspace at start up
+		try {
+			IWorkspace workspace = ResourcesPlugin.getWorkspace();
+			workspace.getRoot().refreshLocal(IResource.DEPTH_INFINITE, null);
+		} catch (CoreException ex) {
+			ex.printStackTrace(); // TODO Use application log
+		}
+	}
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.ui.IWorkbenchListener#preShutdown(org.eclipse.ui.IWorkbench, boolean)
