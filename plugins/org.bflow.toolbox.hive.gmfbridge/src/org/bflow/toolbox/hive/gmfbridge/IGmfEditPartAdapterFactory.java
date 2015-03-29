aa6787747769f7774b08e22f0c54002f6d03989b
astorch <astorch@ee779243-7f51-0410-ad50-ee0809328dc4>
2015-03-29 13:01:03 +0000

    Added source documentation
    git-svn-id: svn+ssh://svn.code.sf.net/p/bflowtoolbox/code/trunk@1087 ee779243-7f51-0410-ad50-ee0809328dc4

--- plugins/org.bflow.toolbox.hive.gmfbridge/src/org/bflow/toolbox/hive/gmfbridge/IGmfEditPartAdapterFactory.java
+++ plugins/org.bflow.toolbox.hive.gmfbridge/src/org/bflow/toolbox/hive/gmfbridge/IGmfEditPartAdapterFactory.java
@@ -3,10 +3,33 @@ package org.bflow.toolbox.hive.gmfbridge;
 import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditor;
 import org.eclipse.ui.IEditorPart;
 
+/**
+ * Factory to create adapters for {@link DiagramEditor}.
+ * 
+ * @author Arian Storch<arian.storch@bflow.org>
+ * @since 27.03.2015
+ * 
+ */
 public interface IGmfEditPartAdapterFactory {
-	
+
+	/**
+	 * Returns TRUE if the factory can create an adapter for the given editor
+	 * part.
+	 * 
+	 * @param editorPart
+	 *            Editor part to adapt
+	 * @return TRUE or FALSE
+	 */
 	boolean canAdapt(IEditorPart editorPart);
-	
+
+	/**
+	 * Returns the adapted instance of {@link DiagramEditor} derived from the
+	 * given editor part.
+	 * 
+	 * @param editorPart
+	 *            Editor part to adapt
+	 * @return Instance of {@link DiagramEditor}
+	 */
 	DiagramEditor getAdapter(IEditorPart editorPart);
 
 }
