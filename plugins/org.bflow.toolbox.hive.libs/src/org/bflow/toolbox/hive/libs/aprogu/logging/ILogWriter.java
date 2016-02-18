ecfcfa51af13784ae946cd73d43167f45cdaed85
Arian Storch <arian.storch@bflow.org>
2016-02-18 18:07:01 +0100

    Added source documentation

--- plugins/org.bflow.toolbox.hive.libs/src/org/bflow/toolbox/hive/libs/aprogu/logging/ILogWriter.java
+++ plugins/org.bflow.toolbox.hive.libs/src/org/bflow/toolbox/hive/libs/aprogu/logging/ILogWriter.java
@@ -1,9 +1,30 @@
 package org.bflow.toolbox.hive.libs.aprogu.logging;
 
+/**
+ * Defines a log writer.
+ * @author Arian Storch<arian.storch@bflow.org>
+ * @since 2016-02-18
+ *
+ */
 public interface ILogWriter {
 	
+	/**
+	 * Writes the given message at error level to the log.
+	 * 
+	 * @param message
+	 *            Message to write
+	 */
 	void error(String message);
 	
+	/**
+	 * Writes the a message using the given message format and arguments at
+	 * error level to the log.
+	 * 
+	 * @param messageFormat
+	 *            Format of the message
+	 * @param args
+	 *            Format arguments
+	 */
 	void errorFormat(String messageFormat, Object...args);
 
 }
