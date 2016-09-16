a31333e311896e2fc89ae8d35ff3285fbf3cf0e7
Arian Storch <arian.storch@bflow.org>
2016-09-16 14:27:30 +0200

    Enhanced code style

--- plugins/org.bflow.toolbox.hive.addons/src/org/bflow/toolbox/hive/addons/core/exceptions/ComponentException.java
+++ plugins/org.bflow.toolbox.hive.addons/src/org/bflow/toolbox/hive/addons/core/exceptions/ComponentException.java
@@ -1,46 +1,53 @@
 package org.bflow.toolbox.hive.addons.core.exceptions;
 
 /**
- * Defines a component exception for mitamm.
+ * Defines a component exception for an Add-on operation.
+ * 
  * @author Arian Storch
  * @since 29/09/09
  * @version 23/09/10
- *
+ * 
  */
 public class ComponentException extends ProtocolException {
 
 	/**
-	 * generated serial version uid
+	 * generated serial version UID
 	 */
 	private static final long serialVersionUID = -4202556327327447033L;
-	
+
 	/**
 	 * Constructor.
-	 * @param msg detail message
+	 * 
+	 * @param msg
+	 *            detail message
 	 */
-	public ComponentException(String msg) 
-	{
+	public ComponentException(String msg) {
 		super(msg);
 	}
-	
+
 	/**
-	 * Constructor.<p/>
+	 * Constructor.
+	 * <p/>
 	 * Wraps a throwable Object or Exception into a ComponentException.
-	 * @param t throwable Object or Exception
+	 * 
+	 * @param t
+	 *            throwable Object or Exception
 	 */
-	public ComponentException(Throwable t)
-	{
+	public ComponentException(Throwable t) {
 		super(t);
 	}
-	
+
 	/**
-	 * Constructor.<p/>
+	 * Constructor.
+	 * <p/>
 	 * Wraps a throwable Object or Exception into a ComponentException.
-	 * @param msg detail message
-	 * @param t throwable Object or Exception
+	 * 
+	 * @param msg
+	 *            detail message
+	 * @param t
+	 *            throwable Object or Exception
 	 */
-	public ComponentException(String msg, Throwable t)
-	{
+	public ComponentException(String msg, Throwable t) {
 		super(msg, t);
 	}
 
