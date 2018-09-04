601e04fd2e134cf103a256a180f32bb9d6522004
Arian Storch <arian.storch@evy-solutions.de>
2018-09-04 12:19:29 +0200

    Added draft of bflow setup

---
+++ install/bflow.setup/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using Ionic.Zip;
+
+namespace bflow.setup {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void OnKlickMichClick(object sender, RoutedEventArgs e) {
+            Console.WriteLine("Ich wurde geklickt");
+        }
+
+        private void DoZip() {
+            using (ZipFile zipFile = ZipFile.Read("foo.zip")) {
+                // TODO extract
+            }
+        }
+    }
+}
