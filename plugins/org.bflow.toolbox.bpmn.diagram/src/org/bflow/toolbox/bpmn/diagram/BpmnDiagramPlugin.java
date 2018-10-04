0e15f7c41ce776175f6f78a347b83b9973ec92e6
Arian Storch <arian.storch@evy-solutions.de>
2018-10-04 17:41:55 +0200

    BPMN diagrams can now be created as EPC, oEPC and so on

--- plugins/org.bflow.toolbox.bpmn.diagram/src/org/bflow/toolbox/bpmn/diagram/BpmnDiagramPlugin.java
+++ plugins/org.bflow.toolbox.bpmn.diagram/src/org/bflow/toolbox/bpmn/diagram/BpmnDiagramPlugin.java
@@ -1,5 +1,7 @@
 package org.bflow.toolbox.bpmn.diagram;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 /**
@@ -10,5 +12,62 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
  *
  */
 public class BpmnDiagramPlugin extends AbstractUIPlugin {
-
+	public static final String ID = "org.bflow.toolbox.bpmn.diagram";
+	private static BpmnDiagramPlugin _instance;
+	
+	/** Initializes the new instance. */
+	public BpmnDiagramPlugin() {
+		_instance = this;
+	}
+	
+	/**
+	 * Logs the given message with an error severity.
+	 * 
+	 * @param message Message to log
+	 */
+	public void logError(String message) {
+		logError(message, null);
+	}
+	
+	/**
+	 * Logs the given message and throwable with an error severity.
+	 * 
+	 * @param message   Message to log
+	 * @param throwable Throwable to log
+	 */
+	public void logError(String message, Exception throwable) {
+		if (message == null && throwable != null) {
+			message = throwable.getMessage();
+		}
+		
+		getLog().log(new Status(IStatus.ERROR, ID, IStatus.OK, message, throwable));
+		debug(message, throwable);
+	}
+	
+	/**
+	 * Writes the given arguments into the console out stream.
+	 * 
+	 * @param message   Message
+	 * @param throwable Throwable
+	 */
+	private void debug(String message, Throwable throwable) {
+		if (!isDebugging()) return;
+		
+		if (message != null) {
+			System.err.println(message);
+		}
+		
+		if (throwable != null) {
+			throwable.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Returns the default instance.
+	 * 
+	 * @return The default instance
+	 */
+	public static BpmnDiagramPlugin getInstance() {
+		return _instance;
+	}
 }
--- plugins/org.bflow.toolbox.bpmn.diagram/src/org/bflow/toolbox/bpmn/diagram/handlers/CreateBpmnDiagramHandler.java
+++ plugins/org.bflow.toolbox.bpmn.diagram/src/org/bflow/toolbox/bpmn/diagram/handlers/CreateBpmnDiagramHandler.java
@@ -1,6 +1,17 @@
 package org.bflow.toolbox.bpmn.diagram.handlers;
 
 import org.eclipse.core.commands.IHandler;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.bflow.toolbox.bpmn.diagram.BpmnDiagramPlugin;
+import org.eclipse.bpmn2.modeler.ui.wizards.BPMN2DiagramWizard;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -14,15 +25,61 @@ import org.eclipse.core.commands.ExecutionException;
  *
  */
 public class CreateBpmnDiagramHandler extends AbstractHandler {
-
 	/*
 	 * (non-Javadoc)
 	 * @see org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.ExecutionEvent)
 	 */
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// TODO add some logic here
+		try {
+			// Collection required environment objects
+			Shell shell = HandlerUtil.getActiveShell(event);
+			IWorkbench workbench = PlatformUI.getWorkbench();
+			ISelection selection = HandlerUtil.getCurrentSelection(event);			
+			StructuredSelection structSelection = (StructuredSelection) selection;
+			
+			// Set up and run model wizard
+			BPMN2DiagramWizard modelWizard = new BPMN2DiagramWizardPatcher();
+			modelWizard.init(workbench, structSelection);
+			
+			WizardDialog wizardDialog = new WizardDialog(shell, modelWizard);
+			wizardDialog.open();
+		} catch(Exception ex) {
+			BpmnDiagramPlugin.getInstance().logError("Error on running BPMN diagram wizard", ex);
+		}
+		
 		return null;
+	}
+	
+	/**
+	 * Extends {@link BPMN2DiagramWizard} to patch a size error when the 
+	 * dialog is being presented via {@link CreateBpmnDiagramHandler}.
+	 * 
+	 * @author Arian Storch<arian.storch@bflow.org>
+	 * @since 2018-10-04
+	 *
+	 */
+	class BPMN2DiagramWizardPatcher extends BPMN2DiagramWizard {
+		/*
+		 * (non-Javadoc)
+		 * @see org.eclipse.bpmn2.modeler.ui.wizards.BPMN2DiagramWizard#createPageControls(org.eclipse.swt.widgets.Composite)
+		 */
+		@Override
+		public void createPageControls(Composite pageContainer) {
+			Point sz = pageContainer.getSize();
+			
+			/* 2018-10-04 AST:
+			 * The BPMN2DiagramWizardPage1 calculates the width of its labels via the 
+			 * pageContainer size. When the page is not being displayed by the Eclipse 
+			 * New Wizard, the size is initially 0. So we set up a default size here 
+			 * to avoid the error.
+			 */			
+			if (sz.x == 0) {
+				pageContainer.setSize(600, 300);
+			}
+			
+			super.createPageControls(pageContainer);
+		}
 	}
 
 }
