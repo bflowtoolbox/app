fa8c40f9cbe3c79242cb388a97437b78ae8d20f6
Arian Storch <arian.storch@evy-solutions.de>
2018-10-07 18:50:33 +0200

    Implemented ConvertToBpmnDiagramAction.doRun(...)

--- plugins/org.bflow.toolbox.bpmn.interop/src/org/bflow/toolbox/bpmn/interop/ConvertToBpmnDiagramAction.java
+++ plugins/org.bflow.toolbox.bpmn.interop/src/org/bflow/toolbox/bpmn/interop/ConvertToBpmnDiagramAction.java
@@ -1,11 +1,32 @@
 package org.bflow.toolbox.bpmn.interop;
 
+import java.io.File;
+
+import org.apache.commons.io.FilenameUtils;
 import org.bflow.toolbox.epc.diagram.edit.parts.EpcEditPart;
+import org.bflow.toolbox.hive.interchange.mif.core.IInterchangeDescriptor;
+import org.bflow.toolbox.hive.interchange.mif.core.InterchangeProcessService;
+import org.bflow.toolbox.hive.interchange.mif.core.InterchangeProcessingException;
+import org.bflow.toolbox.hive.interchange.store.ExportDescriptorStore;
+import org.bflow.toolbox.hive.libs.aprogu.lang.Cast;
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.gef.Request;
 import org.eclipse.gmf.runtime.common.ui.util.IWorkbenchPartDescriptor;
 import org.eclipse.gmf.runtime.diagram.ui.actions.DiagramAction;
+import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditor;
+import org.eclipse.gmf.runtime.diagram.ui.parts.IDiagramWorkbenchPart;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IPathEditorInput;
+import org.eclipse.ui.PlatformUI;
+
 import oepc.diagram.edit.parts.OEPCEditPart;
 
 /**
@@ -53,7 +74,69 @@ public class ConvertToBpmnDiagramAction extends DiagramAction {
 	 */
 	@Override
 	protected void doRun(IProgressMonitor progressMonitor) {
-		System.out.println();
+		// First, select source and target file / folder
+		IDiagramWorkbenchPart workbenchPart = getDiagramWorkbenchPart();
+		DiagramEditor diagramEditor = Cast.as(DiagramEditor.class, workbenchPart);
+		if (diagramEditor == null) return;
+		
+		IEditorInput editorInput = diagramEditor.getEditorInput();
+		IPathEditorInput pathEditorInput = Cast.as(IPathEditorInput.class, editorInput);
+		if (pathEditorInput == null) return;
+		
+		File sourceFile = pathEditorInput.getPath().toFile();
+		File targetFile = getTargetFile(sourceFile);
+		
+		// Check if the target file exists and ask the user how to proceed
+		if (targetFile.exists()) {
+			if (!MessageDialog.openQuestion(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), 
+					"Datei überschreiben?", 
+					"Es existiert bereits eine Datei mit dem gleichen Namen. Soll diese überschrieben werden?")) return;
+		}
+		
+		// Perform the conversion
+		File targetFolder = targetFile.getParentFile();		
+		IInterchangeDescriptor descriptor = ExportDescriptorStore.getExportDescription("BPMN");
+		if (descriptor == null) return;
+		
+		try {
+			InterchangeProcessService.processExport(sourceFile, targetFolder, descriptor);
+		} catch (InterchangeProcessingException ex) {
+			throw new RuntimeException(
+					"Error on running model conversion. Check the details.", 
+					ex);
+		}
+		
+		// Refresh the workspace
+		IFile eclipseFile = ResourcesPlugin.getWorkspace().getRoot().getFile(pathEditorInput.getPath());
+		IContainer eclipseFolder = eclipseFile.getParent();
+		
+		try {
+			// 2018-10-07 AST
+			// Unfortunately this approach is not working
+			eclipseFolder.refreshLocal(IResource.DEPTH_INFINITE, progressMonitor);
+			
+			// This is working, but may have a poor performance
+			ResourcesPlugin.getWorkspace().getRoot().refreshLocal(IResource.DEPTH_INFINITE, progressMonitor);
+		} catch (CoreException ex) {
+			throw new RuntimeException(
+					"Error in refreshing workspace. Check the details", 
+					ex);
+		}
+	}
+	
+	/**
+	 * Returns the file pointer to the target file that is derived from the given
+	 * {@code sourceFile}.
+	 * 
+	 * @param sourceFile Source file
+	 * @return File pointer to the derived target file
+	 */
+	private File getTargetFile(File sourceFile) {
+		String sourceFilePath = sourceFile.getAbsolutePath();
+		String sourceFileName = FilenameUtils.getBaseName(sourceFilePath);
+		String sourceFileDirPath = FilenameUtils.getFullPath(sourceFilePath);
+		String targetFilePath = FilenameUtils.concat(sourceFileDirPath, sourceFileName + ".bpmn");
+		return new File(targetFilePath);
 	}
 	
 	/*
