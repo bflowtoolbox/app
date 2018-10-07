6c802c0eeb94fb3272bb8ba085882ad796fe00ea
Arian Storch <arian.storch@evy-solutions.de>
2018-10-07 14:46:46 +0200

    Renamed PluginActivator to BflowDiagramExtensionsPlugin and moved it to top level package

--- plugins/org.bflow.toolbox.diagram.extensions/src/org/bflow/toolbox/extensions/BflowDiagramExtensionsPlugin.java
+++ plugins/org.bflow.toolbox.diagram.extensions/src/org/bflow/toolbox/extensions/BflowDiagramExtensionsPlugin.java
@@ -1,4 +1,4 @@
-package org.bflow.toolbox.extensions.internal;
+package org.bflow.toolbox.extensions;
 
 import org.bflow.toolbox.extensions.workbench.BflowApplicationWorkbenchListener;
 import org.eclipse.ui.PlatformUI;
@@ -6,15 +6,16 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
- * Extends {@link AbstractUIPlugin} to participate in the lifecycle of Plug-ins. 
+ * Extends {@link AbstractUIPlugin} to participate in the life-cycle of plug-ins. 
  * This plug-in registers listeners that are needed for various contributed features.
  * 
  * @author Arian Storch<arian.storch@bflow.org>
- * @since 14/12/13
+ * @since 2013-12-14
+ * @version 2018-10-07 Renamed. Former name was PluginActivator.
  */
-public class PluginActivator extends AbstractUIPlugin {
+public class BflowDiagramExtensionsPlugin extends AbstractUIPlugin {
 	
-	private BflowApplicationWorkbenchListener fAppWorkbenchListener = new BflowApplicationWorkbenchListener();
+	private BflowApplicationWorkbenchListener _appWorkbenchListener = new BflowApplicationWorkbenchListener();
 	
 	/* (non-Javadoc)
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
@@ -22,7 +23,7 @@ public class PluginActivator extends AbstractUIPlugin {
 	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
-		PlatformUI.getWorkbench().addWorkbenchListener(fAppWorkbenchListener);
+		PlatformUI.getWorkbench().addWorkbenchListener(_appWorkbenchListener);
 	}
 	
 	/* (non-Javadoc)
@@ -30,7 +31,7 @@ public class PluginActivator extends AbstractUIPlugin {
 	 */
 	@Override
 	public void stop(BundleContext context) throws Exception {
-		PlatformUI.getWorkbench().removeWorkbenchListener(fAppWorkbenchListener);
+		PlatformUI.getWorkbench().removeWorkbenchListener(_appWorkbenchListener);
 		super.stop(context);
 	}
 
