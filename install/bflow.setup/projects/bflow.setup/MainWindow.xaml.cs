20ea691e22a1342e52806f2a9ee0dcd786e0308e
Arian Storch <arian.storch@evy-solutions.de>
2018-10-31 17:19:43 +0100

    Initialize data context via XAML

--- install/bflow.setup/projects/bflow.setup/MainWindow.xaml.cs
+++ install/bflow.setup/projects/bflow.setup/MainWindow.xaml.cs
@@ -1,13 +1,10 @@
-﻿using System.Windows;
-
-namespace bflow.setup {
+﻿namespace bflow.setup {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window {
+    public partial class MainWindow {
         public MainWindow() {
             InitializeComponent();
-            DataContext = new MainWindowViewModel();
         }
     }
 }
