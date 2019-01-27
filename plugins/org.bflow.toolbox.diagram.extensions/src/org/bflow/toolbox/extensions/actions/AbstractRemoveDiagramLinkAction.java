4f6c003d8bb2565d05598e0d8384156024f96e1e
Arian Storch <arian.storch@evy-solutions.de>
2019-01-27 15:07:48 +0100

    Introducing AbstractRemoveDiagramLinkAction

---
+++ plugins/org.bflow.toolbox.diagram.extensions/src/org/bflow/toolbox/extensions/actions/AbstractRemoveDiagramLinkAction.java
@@ -0,0 +1,13 @@
+package org.bflow.toolbox.extensions.actions;
+
+/**
+ * Diagram link action base class to remove a diagram link from a model element.
+ * 
+ * @author Arian Storch<arian.storch@bflow.org>
+ * @since 2019-01-27
+ *
+ * @param <TSelectionData> Selection data class
+ */
+public abstract class AbstractRemoveDiagramLinkAction<TSelectionData> extends AbstractDiagramLinkAction<TSelectionData, Void> {
+
+}
