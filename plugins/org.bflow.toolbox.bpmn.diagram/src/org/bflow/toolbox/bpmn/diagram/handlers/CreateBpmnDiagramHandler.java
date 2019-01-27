b536428dddc2796dd0fa833e96c45f7d2cef7db9
Arian Storch <arian.storch@evy-solutions.de>
2019-01-27 21:36:59 +0100

    Migrated to new wizard

--- plugins/org.bflow.toolbox.bpmn.diagram/src/org/bflow/toolbox/bpmn/diagram/handlers/CreateBpmnDiagramHandler.java
+++ plugins/org.bflow.toolbox.bpmn.diagram/src/org/bflow/toolbox/bpmn/diagram/handlers/CreateBpmnDiagramHandler.java
@@ -1,20 +1,18 @@
 package org.bflow.toolbox.bpmn.diagram.handlers;
 
+import org.bflow.toolbox.bpmn.diagram.BpmnDiagramPlugin;
+import org.bflow.toolbox.bpmn.diagram.wizards.BpmnCreationWizard;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.wizard.WizardDialog;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.bflow.toolbox.bpmn.diagram.BpmnDiagramPlugin;
-import org.eclipse.bpmn2.modeler.ui.wizards.BPMN2DiagramWizard;
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
 
 /**
  * Implements {@link IHandler} to create a new BPMN diagram when button 
@@ -22,6 +20,7 @@ import org.eclipse.core.commands.ExecutionException;
  * 
  * @author Arian Storch<arian.storch@bflow.org>
  * @since 2018-10-04
+ * @version 2019-01-27 AST Changed to new {@link BpmnCreationWizard}
  *
  */
 public class CreateBpmnDiagramHandler extends AbstractHandler {
@@ -39,7 +38,7 @@ public class CreateBpmnDiagramHandler extends AbstractHandler {
 			StructuredSelection structSelection = (StructuredSelection) selection;
 			
 			// Set up and run model wizard
-			BPMN2DiagramWizard modelWizard = new BPMN2DiagramWizardPatcher();
+			BpmnCreationWizard modelWizard = new BpmnCreationWizard();
 			modelWizard.init(workbench, structSelection);
 			
 			WizardDialog wizardDialog = new WizardDialog(shell, modelWizard);
@@ -50,36 +49,4 @@ public class CreateBpmnDiagramHandler extends AbstractHandler {
 		
 		return null;
 	}
-	
-	/**
-	 * Extends {@link BPMN2DiagramWizard} to patch a size error when the 
-	 * dialog is being presented via {@link CreateBpmnDiagramHandler}.
-	 * 
-	 * @author Arian Storch<arian.storch@bflow.org>
-	 * @since 2018-10-04
-	 *
-	 */
-	class BPMN2DiagramWizardPatcher extends BPMN2DiagramWizard {
-		/*
-		 * (non-Javadoc)
-		 * @see org.eclipse.bpmn2.modeler.ui.wizards.BPMN2DiagramWizard#createPageControls(org.eclipse.swt.widgets.Composite)
-		 */
-		@Override
-		public void createPageControls(Composite pageContainer) {
-			Point sz = pageContainer.getSize();
-			
-			/* 2018-10-04 AST:
-			 * The BPMN2DiagramWizardPage1 calculates the width of its labels via the 
-			 * pageContainer size. When the page is not being displayed by the Eclipse 
-			 * New Wizard, the size is initially 0. So we set up a default size here 
-			 * to avoid the error.
-			 */			
-			if (sz.x == 0) {
-				pageContainer.setSize(600, 300);
-			}
-			
-			super.createPageControls(pageContainer);
-		}
-	}
-
 }
