7e05a54b3a98c7851e28614fab8ae03e07a4d29a
Arian Storch <arian.storch@evy-solutions.de>
2019-02-17 19:28:48 +0100

    Added PluginActivator to bundle org.bflow.toolbox.hive.libs

---
+++ plugins/org.bflow.toolbox.hive.libs/src/org/bflow/toolbox/hive/libs/PluginActivator.java
@@ -0,0 +1,17 @@
+package org.bflow.toolbox.hive.libs;
+
+import org.eclipse.core.runtime.Plugin;
+
+/**
+ * Extends {@link Plugin} to control/hook the life-cycle of this plug-in.
+ * 
+ * @author Arian Storch<arian.storch@bflow.org>
+ * @since 2019-02-17
+ *
+ */
+public class PluginActivator extends Plugin {
+	/** Initializes the new instance. */
+	public PluginActivator() {
+		// Currently nothing to do here
+	}
+}
