1d21d3fc901217f5dbeb8c63d84e66146b216d1c
Arian Storch <arian.storch@evy-solutions.de>
2019-02-23 16:33:36 +0100

    Set log file path by querying the workspace root

--- plugins/org.bflow.toolbox.hive.libs/src/org/bflow/toolbox/hive/libs/PluginActivator.java
+++ plugins/org.bflow.toolbox.hive.libs/src/org/bflow/toolbox/hive/libs/PluginActivator.java
@@ -1,5 +1,6 @@
 package org.bflow.toolbox.hive.libs;
 
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Plugin;
 
 /**
@@ -7,11 +8,21 @@ import org.eclipse.core.runtime.Plugin;
  * 
  * @author Arian Storch<arian.storch@bflow.org>
  * @since 2019-02-17
+ * @version 2019-02-23 AST Set env var `rws_loc` by querying the workspace root
  *
  */
 public class PluginActivator extends Plugin {
 	/** Initializes the new instance. */
 	public PluginActivator() {
-		// Currently nothing to do here
+		setLogFilePath();		
+	}
+	
+	/**
+	 * Sets the log file path by setting the corresponding environment variable to
+	 * the path of the workspace root.
+	 */
+	private void setLogFilePath() {
+		String workspacePath = ResourcesPlugin.getWorkspace().getRoot().getLocation().toOSString();
+		System.setProperty("rws_loc", workspacePath);
 	}
 }
